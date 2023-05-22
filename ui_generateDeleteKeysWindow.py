bb393b6d96a39c29ae5de4920fed2d9a6a9638e6
Jelena Lučić <lj190268d@student.etf.bg.ac.rs>
2023-05-22 02:18:03 +0200

    Created new window files - `GenerateKeyWindow`

--- ui_generateDeleteKeysWindow.py
+++ ui_generateDeleteKeysWindow.py
@@ -15,24 +15,53 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QSizePolicy,
-    QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton,
+    QSizePolicy, QVBoxLayout, QWidget)
 
 class Ui_GenerateDeleteKeysWindow(object):
     def setupUi(self, GenerateDeleteKeysWindow):
         if not GenerateDeleteKeysWindow.objectName():
             GenerateDeleteKeysWindow.setObjectName(u"GenerateDeleteKeysWindow")
         GenerateDeleteKeysWindow.resize(800, 600)
+        GenerateDeleteKeysWindow.setStyleSheet(u"background-color: rgb(51, 51, 51);\n"
+"color: white;\n"
+"font-size: 12pt;")
         self.centralwidget = QWidget(GenerateDeleteKeysWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.titleLabel = QLabel(self.centralwidget)
+        self.titleLabel.setObjectName(u"titleLabel")
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.titleLabel.sizePolicy().hasHeightForWidth())
+        self.titleLabel.setSizePolicy(sizePolicy)
+        self.titleLabel.setMaximumSize(QSize(16777215, 40))
+        self.titleLabel.setStyleSheet(u"background-color: rgb(51, 51, 51);\n"
+"color: white;\n"
+"font-size: 26pt;")
+        self.titleLabel.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.titleLabel)
+
+        self.generateKeyButton = QPushButton(self.centralwidget)
+        self.generateKeyButton.setObjectName(u"generateKeyButton")
+        sizePolicy.setHeightForWidth(self.generateKeyButton.sizePolicy().hasHeightForWidth())
+        self.generateKeyButton.setSizePolicy(sizePolicy)
+        self.generateKeyButton.setStyleSheet(u"background-color: rgb(74, 74, 74);")
+
+        self.verticalLayout.addWidget(self.generateKeyButton)
+
+        self.deleteKeyButton = QPushButton(self.centralwidget)
+        self.deleteKeyButton.setObjectName(u"deleteKeyButton")
+        sizePolicy.setHeightForWidth(self.deleteKeyButton.sizePolicy().hasHeightForWidth())
+        self.deleteKeyButton.setSizePolicy(sizePolicy)
+        self.deleteKeyButton.setStyleSheet(u"background-color: rgb(74, 74, 74);")
+
+        self.verticalLayout.addWidget(self.deleteKeyButton)
+
         GenerateDeleteKeysWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(GenerateDeleteKeysWindow)
-        self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 37))
-        GenerateDeleteKeysWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(GenerateDeleteKeysWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        GenerateDeleteKeysWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(GenerateDeleteKeysWindow)
 
@@ -40,6 +69,9 @@ class Ui_GenerateDeleteKeysWindow(object):
     # setupUi
 
     def retranslateUi(self, GenerateDeleteKeysWindow):
-        GenerateDeleteKeysWindow.setWindowTitle(QCoreApplication.translate("GenerateDeleteKeysWindow", u"MainWindow", None))
+        GenerateDeleteKeysWindow.setWindowTitle(QCoreApplication.translate("GenerateDeleteKeysWindow", u"Generate and delete keys", None))
+        self.titleLabel.setText(QCoreApplication.translate("GenerateDeleteKeysWindow", u"Here you can generate and delete your keys", None))
+        self.generateKeyButton.setText(QCoreApplication.translate("GenerateDeleteKeysWindow", u"Generate key", None))
+        self.deleteKeyButton.setText(QCoreApplication.translate("GenerateDeleteKeysWindow", u"Delete key", None))
     # retranslateUi
 
