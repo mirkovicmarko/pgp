7c712a27d0cb05332b636b3abfc419861da1338d
Jelena Lučić <lj190268d@student.etf.bg.ac.rs>
2023-05-22 02:18:49 +0200

    Connected `GenerateKeyWindow` with generate key button.

--- generateDeleteKeysWindow.py
+++ generateDeleteKeysWindow.py
@@ -2,8 +2,14 @@ from PySide6.QtWidgets import QMainWindow
 
 from ui_generateDeleteKeysWindow import Ui_GenerateDeleteKeysWindow
 
+from windowManager import WindowManager
+from generateKeyWindow import GenerateKeyWindow
+
+
 class GenerateDeleteKeysWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_GenerateDeleteKeysWindow()
         self.ui.setupUi(self)
+
+        self.ui.generateKeyButton.clicked.connect(lambda: WindowManager.openWindow(self, GenerateKeyWindow))
